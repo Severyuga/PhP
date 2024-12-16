0ee8953b503fc2c6c48fc0fc9cf228608c90db45
Lana <severseveryuga@gmail.com>
2024-12-16 09:16:39 +0300

    д

--- l8/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Константы для подключения к базе данных
-define('DB_NAME', 'f1034565_login_db'); // Имя базы данных
-define('DB_USER', 'f1034565_login_db'); // Имя пользователя базы данных
-define('DB_PASSWORD', 'oa7KHZbC'); // Пароль пользователя базы данных
-define('DB_HOST', 'localhost'); // Хост базы данных
-define('DB_CHARSET', 'utf8'); // Кодировка базы данных
-?>
