e285de439f43ccde257e2894b4293ac96ed1fff0
Lana <severseveryuga@gmail.com>
2024-12-16 11:09:48 +0300

    k

--- l6/session/savepage.inc.php
+++ l6/session/savepage.inc.php
@@ -3,18 +3,17 @@ declare(strict_types=1);
 // Код для всех страниц - сохранение информации о посещенных страницах
 /*
 ЗАДАНИЕ 1
-- Создайте в сессии либо 
-	- массив для хранения всех посещенных страниц и сохраните в качестве его очередного элемента путь к текущей странице. 
-	- строку с уникальным разделителем и последовательно её дополняйте
+- Создайте в сессии либо
+- массив для хранения всех посещенных страниц и сохраните в качестве его очередного элемента путь к текущей странице.
+- строку с уникальным разделителем и последовательно её дополняйте
 
 */
 
 if (!isset($_SESSION['visitedPages']))
-	$_SESSION['visitedPages'] = [];
+$_SESSION['visitedPages'] = [];
 
-
-//$currentPage = basename($_SERVER['PHP_SELF']);
-$currentPage = $_SERVER['REQUEST_URI'];
+$currentPage = $_SERVER['PHP_SELF'];
+//$currentPage = $_SERVER['REQUEST_URI'];
 
 //if (!in_array($currentPage, $_SESSION['visitedPages']))
 $_SESSION['visitedPages'][] = $currentPage;
